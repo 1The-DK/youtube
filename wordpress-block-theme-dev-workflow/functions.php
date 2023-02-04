f0ac70fced03706b65f761f5b0adb64a1697de58
baillie <hello@baillieogrady.com>
2023-02-04 17:16:32 +0000

    Add WordPress block theme dev workflow

---
+++ wordpress-block-theme-dev-workflow/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+* Enqueue scripts and styles.
+*/
+add_action( 'wp_enqueue_scripts', function() {
+    wp_enqueue_style( 'app.css', get_template_directory_uri() . '/dist/app.css');
+    wp_enqueue_script( 'app.js', get_template_directory_uri() . '/dist/app.js');
+});
